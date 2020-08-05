58975fbdebe11a0b6397ad0c4e7c18826cd6f3d9
Yee-Ting Li <yee379@gmail.com>
2020-08-05 00:11:54 -0700

    updates for logging

--- collab-listener.py
+++ collab-listener.py
@@ -5,10 +5,13 @@ from json import loads
 import subprocess
 import os
 import functools
+import requests
+import timeit
 
 TOPIC = os.environ['KAFKA_TOPIC']
 GROUPID = os.environ['KAFKA_GROUP_ID']
 BOOTSTRAP = os.environ['KAFKA_BOOTSTRAP_SERVER']
+LOGBOOK_URI = os.environ['LOGBOOK_URI']
 
 # flush all prints
 print = functools.partial(print, flush=True)
@@ -24,17 +27,32 @@ consumer = KafkaConsumer(
     bootstrap_servers=[os.environ['KAFKA_BOOTSTRAP_SERVER']]
 )
 
+def setfacl( userids, directory ):
+
+    acl = [ f'u:{u}:r-x,default:u:{u}:r-x' for u in userids ]
+    cmd = f"setfacl -R --set user::rwx,group::rwx,other::---,default:mask:rwx,default:other:---,{','.join(acl)}  {directory}"
+    print( f'  {cmd}' )
+    output = subprocess.check_output( cmd, shell=True, stderr=subprocess.STDOUT, ).decode(encoding="utf-8")
+    s = True if output == '' else False
+    return s
+
+def wrapper(func, *args, **kwargs):
+    def wrapped():
+        return func(*args, **kwargs)
+    return wrapped
+
 
 for message in consumer:
 
-    print(message.value)
+    #print(message.value)
 
     op = message.value['CRUD']
     directory = message.value['experiment_name']
-    directory = '.'
+    prefix = message.value['experiment_name'][0:6]
+    directory = os.environ['EXP_DIR'] + '/' + prefix + '/' +  message.value['experiment_name'] + '_' + message.value['instrument'] + '/'
 
     m = message.value['value']
-    print( f"Permissions update on directory {directory} NEED INSTRUMENT {op} with {m['players']} by {m['requestor']}" )
+    print( f"Permissions update on directory {directory} {op} with {m['players']} by {m['requestor']}" )
 
     # get current
     output = subprocess.check_output(
@@ -48,18 +66,18 @@ for message in consumer:
     users = [ u.split(':')[-1] for u in m['players'] if u.startswith('uid:') ]
     #print(f"SET TO USERS {users}")
 
-    print(f"  {current_users}\t->{users}")
-    
-    add_users = set(users) - set(current_users)
-    add = [ 'u:%s:rx' % u for u in add_users ]
-    if len(add_users) > 0:
-        cmd = "setfacl %s -Rm o::---  %s -Rdm o::---  %s" % (' '.join( [ '-Rm %s'%u for u in add] ), ' '.join( [ '-Rdm %s'%u for u in add] ), directory )
-        print(f"  ADD: {cmd}")
+    # get uids from names
+    names = '&'.join( [ f'uid={user}' for user in users ] )
+    url = f'{LOGBOOK_URI}/cryoem-data/lgbk/ws/get_matching_uids?{names}'
+    response = requests.get(url, auth=(os.environ['USERNAME'].strip(), os.environ['PASSWORD'].strip())).json()
+    user_map = {}
+    for u in response['value']:
+        user_map[ u['uid'] ] = u['uidNumber']
+    userids = [ user_map[u] for u in users ]
+    print(f"  current: {current_users}\tnew ->\t{userids}")
 
-    del_users = set(current_users) - set(users)
-    delete = [ 'u:%s'%u for u in del_users ]
-    if len(del_users) > 0:
-        cmd = "setfacl %s  %s  %s" % (' '.join( [ '-Rx %s'%u for u in delete] ), ' '.join( [ '-Rdx %s'%u for u in delete] ), directory )
-        print(f"  DEL: {cmd}")
+    wrapped = wrapper( setfacl, userids, directory )
+    t = timeit.timeit(wrapped, number=1)
+    print( f'  done in {int(t)}s' )
 
     print()
